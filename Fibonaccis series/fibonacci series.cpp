e37e4d0c1be90e330aedd13df19f3cf39d23aa3e
Elgun Eminov <68548878+elguneminov@users.noreply.github.com>
2021-07-21 21:37:51 +0400

    Add files via upload

---
+++ Fibonaccis series/fibonacci series.cpp
@@ -0,0 +1,34 @@
+// To print Fibonacci series up to n number of times
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int n, t1 = 0, t2 = 1, nextTerm = 0;
+    cout<<"Enter number of terms: ";
+    cin>>n;
+
+    cout<<"Fibonacci series:";
+
+    for(int i = 1; i <= n; ++i)
+    {
+        if(i == 1) // to print first 2 terms
+        {
+            cout<<" "<<t1<<" ";
+            continue;
+        }
+        if(i == 2)
+        {
+            cout<<t2<<" ";
+            continue;
+        }
+        nextTerm = t1 + t2;
+        t1 = t2;
+        t2 = nextTerm;
+
+        cout<<nextTerm<<" ";
+    }
+    return 0;
+}
